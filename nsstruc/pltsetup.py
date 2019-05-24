68e38bc193f1de609b3d13f2b09add13078fb466
Phil <plandry@uoguelph.ca>
2019-05-24 15:55:11 -0500

    Adding csv plotter

--- nsstruc/pltsetup.py
+++ nsstruc/pltsetup.py
@@ -9,3 +9,5 @@ plt.rcParams['axes.color_cycle'] = ['black', 'limegreen', 'darkgrey', 'orange', 
 plt.rcParams['axes.labelsize'] = 28
 plt.rcParams['figure.figsize'] = 10, 10
 AUTO_COLORS =  plt.rcParams['axes.color_cycle']
+AUTO_MARKERS = ['.', '+', 'x', 'd', '1']
+AUTO_LINESTYLES = ['-', '--', ':', '-.']
