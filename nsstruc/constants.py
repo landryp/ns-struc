df15ffd8d59678eaabbcfb5c5b9e85b92ad2966f
Reed Essick <reed.essick@ligo.org>
2020-01-09 10:38:46 -0600

    changed the way constants were defined to add a few more terms

--- nsstruc/constants.py
+++ nsstruc/constants.py
@@ -1,7 +1,20 @@
-#!/usr/bin/python
+"definition of useful physical constants"
+__author__ = "philippe.landry@ligo.org and Reed Essick (reed.essick@gmail.com)"
+
+#-------------------------------------------------
+
+from numpy import pi
+
+#-------------------------------------------------
+
+twopi = 2.*pi
+fourpi = 4.*pi
 
 c = 2.99792458e10 # speed of light in cgs
+c2 = c**2
 G = 6.67408e-8 # Newton's constant in cgs
+
+G_c2 = G/c2
+
 Msun = 1.3271244e26/G # solar mass in cgs
 rhonuc = 2.8e14 # nuclear density in cgs
-hbar = 1.0545716e-27 # reduced Planck constant in cgs
