71583cbc1d1b72214d7b0aed227c18ecce7d766d
Philippe Landry <philippe.landry@ldas-pcdev1.ligo.caltech.edu>
2021-10-13 10:00:47 -0700

    edit pltsetup

--- nsstruc/pltsetup.py
+++ nsstruc/pltsetup.py
@@ -5,9 +5,9 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoLocator
 
-plt.rcParams['axes.color_cycle'] = ['black', 'limegreen', 'darkgrey', 'orange', 'cyan', 'hotpink', 'blue', 'darkred']
+#plt.rcParams['axes.color_cycle'] = ['black', 'limegreen', 'darkgrey', 'orange', 'cyan', 'hotpink', 'blue', 'darkred']
 plt.rcParams['axes.labelsize'] = 28
 plt.rcParams['figure.figsize'] = 10, 10
-AUTO_COLORS =  plt.rcParams['axes.color_cycle']
+AUTO_COLORS = ['black', 'limegreen', 'darkgrey', 'orange', 'cyan', 'hotpink', 'blue', 'darkred']  #plt.rcParams['axes.color_cycle']
 AUTO_MARKERS = ['.', '+', 'x', 'd', '1']
 AUTO_LINESTYLES = ['-', '--', ':', '-.']
